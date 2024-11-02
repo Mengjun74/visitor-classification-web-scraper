a0708d20c73e0049d1aebbdf525b2e41f93df018
Mengjun Chen <91912728+Mengjun74@users.noreply.github.com>
2024-11-01 17:36:52 -0700

    Add files via upload

--- machine_learning/gpt_api/gpt_model.py
+++ machine_learning/gpt_api/gpt_model.py
@@ -1,1 +1,23 @@
-#Chatgpt API method
+from openai import OpenAI
+
+client = OpenAI(
+    api_key="api-key",
+)
+
+initial = """
+        "Given the following website content, classify it into a category. "
+        "Provide only the category name without any reasoning or explanation. "
+        "Content: '{}'"
+"""
+
+def llm_response(initial_prompt = initial, content = None):
+    response = client.chat.completions.create(
+    messages=[
+        {"role":"system", "content": initial_prompt},
+        {"role": "user", "content": f"Classify the following content: {content}"},
+    ],
+    model="gpt-4o-mini",
+    max_tokens=50, 
+    temperature=0.5
+    )
+    return response.choices[0].message.content.strip()
